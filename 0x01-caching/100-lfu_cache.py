a59961c431955b58c4991591507719134ea11878
NancyLwambi <99336401+NancyLwambi@users.noreply.github.com>
2023-01-25 16:27:00 +0300

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,49 +1,72 @@
 #!/usr/bin/python3
 """ LFU Caching """
+
 from base_caching import BaseCaching
-from collections import OrderedDict
 
 
 class LFUCache(BaseCaching):
-    """ Class that inherits from BaseCaching and is a caching system """
+    """ LFU caching """
+
     def __init__(self):
+        """ Constructor """
         super().__init__()
-        self.lru_cache = OrderedDict()
-        self.lfu_cache = {}
+        self.queue = []
+        self.counter = {}
 
     def put(self, key, item):
-        """ Assign to the dictionary, LFU algorithm """
-        if key in self.lru_cache:
-            del self.lru_cache[key]
-        if len(self.lru_cache) > BaseCaching.MAX_ITEMS - 1:
-            min_value = min(self.lfu_cache.values())
-            lfu_keys = [k for k, v in self.lfu_cache.items() if v == min_value]
-            if len(lfu_keys) == 1:
-                print("DISCARD:", lfu_keys[0])
-                self.lru_cache.pop(lfu_keys[0])
-                del self.lfu_cache[lfu_keys[0]]
-            else:
-                for k, _ in list(self.lru_cache.items()):
-                    if k in lfu_keys:
-                        print("DISCARD:", k)
-                        self.lru_cache.pop(k)
-                        del self.lfu_cache[k]
-                        break
-        self.lru_cache[key] = item
-        self.lru_cache.move_to_end(key)
-        if key in self.lfu_cache:
-            self.lfu_cache[key] += 1
+        """ Puts item in cache """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        item_count = self.counter.get(key, None)
+
+        if item_count is not None:
+            self.counter[key] += 1
         else:
-            self.lfu_cache[key] = 1
-        self.cache_data = dict(self.lru_cache)
+            self.counter[key] = 1
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = self.get_first_list(self.queue)
+            if first:
+                self.queue.pop(0)
+                del self.cache_data[first]
+                del self.counter[first]
+                print("DISCARD: {}".format(first))
+
+        if key not in self.queue:
+            self.queue.insert(0, key)
+        self.mv_right_list(key)
 
     def get(self, key):
-        """ Return the value linked """
-        if key in self.lru_cache:
-            value = self.lru_cache[key]
-            self.lru_cache.move_to_end(key)
-            if key in self.lfu_cache:
-                self.lfu_cache[key] += 1
-            else:
-                self.lfu_cache[key] = 1
-            return value    
+        """ Gets item from cache """
+        item = self.cache_data.get(key, None)
+        if item is not None:
+            self.counter[key] += 1
+            self.mv_right_list(key)
+        return item
+
+    def mv_right_list(self, item):
+        """ Moves element to the right, taking into account LFU """
+        length = len(self.queue)
+
+        idx = self.queue.index(item)
+        item_count = self.counter[item]
+
+        for i in range(idx, length):
+            if i != (length - 1):
+                nxt = self.queue[i + 1]
+                nxt_count = self.counter[nxt]
+
+                if nxt_count > item_count:
+                    break
+
+        self.queue.insert(i + 1, item)
+        self.queue.remove(item)
+
+    @staticmethod
+    def get_first_list(array):
+        """ Get first element of list or None """
+        return array[0] if array else None
+    
